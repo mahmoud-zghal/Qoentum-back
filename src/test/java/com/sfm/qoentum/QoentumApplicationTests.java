c84ae2da7ac73377e93b9843280ee44eba0971f2
mahmoud <mahmoud.business99@gmail.com>
2022-07-01 10:08:07 +0100

    first commit

---
+++ src/test/java/com/sfm/qoentum/QoentumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfm.qoentum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QoentumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
